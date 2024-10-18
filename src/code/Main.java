1eccbe9896a6d93cca8342f67472f654c1a320b2
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2024-10-18 14:31:54 +0300

    Added performance measure for CPU utilization and RAM usage.

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,6 +1,8 @@
 package code;
 
 import code.entites.Node;
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
 import code.entites.State;
 import code.search.AStar;
 import code.search.BFS;
@@ -9,23 +11,20 @@ import code.search.GeneralSearch;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 
 public class Main {
 
     public static void main(String[] args) {
-//        System.out.println("Hello, World!");
+    	OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(1);
         pq.add(2);
         pq.add(4);
         pq.add(3);
-//        System.out.println(pq.poll());
-
-//        GeneralSearch aStar = new AStar();
-//        // use the evalFn to print the result of EVAL_Fn
-//        System.out.println(aStar.evalFn.EVAL_Fn(null,1));
-
-
 
         String p1 = "5;4;" + "b,y,r,b;" + "b,y,r,r;" + "y,r,b,y;" + "e,e,e,e;" + "e,e,e,e;";
         String p2 = "3;4;" + "b,b,b,r;" + "r,r,r,b;" + "e,e,e,e;";
@@ -37,8 +36,41 @@ public class Main {
                 "y,r,y,r;" +
                 "e,e,e,e;";
 
+        String grid1= "5;" +
+                "4;" +
+                "b,y,r,b;" +
+                "b,y,r,r;" +
+                "y,r,b,y;" +
+                "e,e,e,e;" +
+                "e,e,e,e;";
+        String grid2 = "5;" +
+                "4;" +
+                "b,r,o,b;" +
+                "b,r,o,o;" +
+                "r,o,b,r;" +
+                "e,e,e,e;" +
+                "e,e,e,e;";
+        String grid3 = "6;" +
+                "4;" +
+                "g,g,g,r;" +
+                "g,y,r,o;" +
+                "o,r,o,y;" +
+                "y,o,y,b;" +
+                "r,b,b,b;" +
+                "e,e,e,e;";
+        String grid4 = "6;" +
+                "3;" +
+                "r,r,y;" +
+                "b,y,r;" +
+                "y,b,g;" +
+                "g,g,b;" +
+                "e,e,e;" +
+                "e,e,e;";
 
-        System.out.println(WaterSortSearch.solve(grid0, "UC", true));
+        
+        System.out.println(WaterSortSearch.solve(grid4, "UCS", true));
+  
+        
         Node cur = WaterSortSearch.currentAgent.getSolutionNode();
         ArrayList<State> steps = new ArrayList<>();
         while (cur != null) {
@@ -46,6 +78,12 @@ public class Main {
             cur = cur.getParent();
         }
         WaterSortVisualizer visualizer = new WaterSortVisualizer(steps);
-//        System.out.println(waterSortSearch.solve(p2, "DF", false));
+
+        double memoryUsed = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1_000_000;
+        double cpuUtilization = osBean.getProcessCpuLoad() * 100;
+        System.out.println("Memeroy used: " + memoryUsed + " MB");
+        System.out.println("CPU Utilization: " + String.format("%.2f", cpuUtilization)  + "%");
     }
+    
+    
 }
